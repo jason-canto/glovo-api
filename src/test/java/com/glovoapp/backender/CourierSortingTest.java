6b9ef4eed9078f082931c2c1ac07fb7a00753815
jason.canto <rofsdg@gmail.com>
2018-09-27 00:24:29 -0300

    Adding tests

--- src/test/java/com/glovoapp/backender/CourierSortingTest.java
+++ src/test/java/com/glovoapp/backender/CourierSortingTest.java
@@ -1,18 +1,37 @@
 package com.glovoapp.backender;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import com.glovoapp.backender.repository.CourierRepository;
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class CourierSortingTest {
 
-	@Mock
-	private CourierRepository courierRepository;
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
-	void testOrdersByCourier() {
-		
+	public void testGetAllOrders() throws Exception {
+		mvc.perform(get("/orders")
+				.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content()
+				.contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$", hasSize(2)));
+
 	}
 
 }
